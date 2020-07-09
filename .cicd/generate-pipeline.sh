e97c7765dead7ab1d5efc63e5f3e653aff176400
Scott Arnette <scott.a.arnette@gmail.com>
2020-07-09 16:55:31 -0400

    Use test branch of mac-anka-fleet.

--- .cicd/generate-pipeline.sh
+++ .cicd/generate-pipeline.sh
@@ -155,7 +155,7 @@ EOF
             - 'registry_1'
             - 'registry_2'
           pre-commands: 
-            - "rm -rf mac-anka-fleet; git clone git@github.com:EOSIO/mac-anka-fleet.git && cd mac-anka-fleet && . ./ensure-tag.bash -u 12 -r 25G -a '-n'"
+            - "rm -rf mac-anka-fleet; git clone git@github.com:EOSIO/mac-anka-fleet.git -b htt-gitupdate && cd mac-anka-fleet && . ./ensure-tag.bash -u 12 -r 25G -a '-n'"
       - EOSIO/skip-checkout#v0.1.1:
           cd: ~
     env:
