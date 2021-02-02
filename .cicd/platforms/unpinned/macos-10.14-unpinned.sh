8bc4cd30d942b60bc3d198df2029f4151051c253
Scott Arnette <scott.a.arnette@gmail.com>
2021-02-02 14:28:17 -0500

    Try kicking the Mojave VMs for develop-boxed.

--- .cicd/platforms/unpinned/macos-10.14-unpinned.sh
+++ .cicd/platforms/unpinned/macos-10.14-unpinned.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -eo pipefail
-VERSION=1
+VERSION=2
 brew update
 brew install git cmake python libtool libusb graphviz automake wget gmp pkgconfig doxygen openssl@1.1 jq boost libpq libpqxx postgres rabbitmq || :
 # install nvm for ship_test
